4d21102771aa567a9d1fcebdb6f1d96ead74567d
ethand9 <43592552+ethand9@users.noreply.github.com>
2018-12-01 15:54:22 -0800

    added classes to plant titles

--- public/index.js
+++ public/index.js
@@ -4,3 +4,54 @@
  *
 */
 
+allPlants = document.querySelectorAll('.nav-bar')
+
+function functionName(){
+    console.log("a button was clicked.")
+}
+
+function waterPlant(){
+    console.log("watering")
+    // unhide watering can that waters the plant
+    // change plant picture to bigger plant
+    // update water progress
+}
+
+function fertilizePlant(){
+    console.log("fert")
+    // unhide fertilizer bag
+}
+
+function sunlightPlant(){
+    console.log("sun")
+    // unhide sunlight
+}
+
+function renamePlant(){
+    console.log("rename")
+    console.log(allPlants)
+    
+}
+
+function aboutPlant(){
+    console.log("about")
+    // show description
+}
+
+function uprootPlant(){
+    console.log("uproot")
+    // delete the plant
+}
+
+function addPlant(){
+    console.log("add plant")
+    // new plant
+}
+
+document.getElementById('water').addEventListener('click', waterPlant);
+document.getElementById('fertilize').addEventListener('click', fertilizePlant);
+document.getElementById('sunlight').addEventListener('click', sunlightPlant);
+document.getElementById('rename').addEventListener('click', renamePlant);
+document.getElementById('about').addEventListener('click', aboutPlant);
+document.getElementById('uproot').addEventListener('click', uprootPlant);
+document.getElementById('add-plant').addEventListener('click', addPlant);
