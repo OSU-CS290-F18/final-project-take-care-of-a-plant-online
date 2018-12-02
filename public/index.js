28fa03addfa0703f4a25b10d3d9d4d7d23e2c496
ethand9 <43592552+ethand9@users.noreply.github.com>
2018-12-01 17:52:45 -0800

    added plant, changed some css/html

--- public/index.js
+++ public/index.js
@@ -8,6 +8,7 @@ var allPlantsTitle = document.querySelectorAll('.plant-title');
 var allPlantsContainerNode = document.querySelectorAll('.plant-and-button');
 var allPlantsContainer = Array.prototype.slice.call(allPlantsContainerNode); // convert the node to an array; https://davidwalsh.name/nodelist-array
 var currentPlantIndex = 0;
+var allPlantsContainerSection = document.getElementById('all-plants')
 
 function changeCurrentIndex(event) { // changes the index when switching plants
     for(var i = allPlantsTitle.length - 1; i >= 0; i--){
@@ -56,7 +57,7 @@ function addPlant() { // new plant
     // var plantName = document.getElementById('').value;
     // var plantImage = document.getElementById('').value;
     var plantName = "My Plant";
-    var plantImage = "https://www.ikea.com/PIAimages/0614197_PE686822_S5.JPG";
+    var plantImageSource = "Plant_for_Final_Project.jpg";
 
     // plant and button div
     var plantAndButtonContainer = document.createElement('div');
@@ -66,50 +67,97 @@ function addPlant() { // new plant
     var plantImageContainer = document.createElement('div');
     var plantImage = document.createElement('img');
     plantImageContainer.classList.add('plant-image');
-    // postImg.setAttribute('src', postPhotoUrl);
-    // postImg.setAttribute('alt', postTitleText);
+    plantImage.setAttribute('src', plantImageSource);
+    plantImage.setAttribute('alt', plantName);
+    plantImageContainer.appendChild(plantImage);
+    plantAndButtonContainer.appendChild(plantImageContainer);
 
     // buttons section
     var buttonSection = document.createElement('section');
+    buttonSection.classList.add('tool-buttons')
+    plantAndButtonContainer.appendChild(buttonSection);
 
     // water
     var outterButtonDiv1 = document.createElement('div');
     var innerButtonDiv1 = document.createElement('div');
     var button1  = document.createElement('button');
-
+    outterButtonDiv1.classList.add('button');
+    innerButtonDiv1.classList.add('button-contents');
+    button1.classList.add('water');
+    button1.textContent = "Water";
+    innerButtonDiv1.appendChild(button1);
+    outterButtonDiv1.appendChild(innerButtonDiv1);
+    buttonSection.appendChild(outterButtonDiv1);
+    
     // fertilize
     var outterButtonDiv2 = document.createElement('div');
     var innerButtonDiv2 = document.createElement('div');
     var button2  = document.createElement('button');
+    outterButtonDiv2.classList.add('button');
+    innerButtonDiv2.classList.add('button-contents');
+    button2.classList.add('fertilize');
+    button2.textContent = "Fertilize";
+    innerButtonDiv2.appendChild(button2);
+    outterButtonDiv2.appendChild(innerButtonDiv2);
+    buttonSection.appendChild(outterButtonDiv2);
 
     // sunlight
     var outterButtonDiv3 = document.createElement('div');
     var innerButtonDiv3 = document.createElement('div');
     var button3  = document.createElement('button');
+    outterButtonDiv3.classList.add('button');
+    innerButtonDiv3.classList.add('button-contents');
+    button3.classList.add('sunlight');
+    button3.textContent = "Sunlight";
+    innerButtonDiv3.appendChild(button3);
+    outterButtonDiv3.appendChild(innerButtonDiv3);
+    buttonSection.appendChild(outterButtonDiv3);
 
     // rename
     var outterButtonDiv4 = document.createElement('div');
     var innerButtonDiv4 = document.createElement('div');
     var button4  = document.createElement('button');
+    outterButtonDiv4.classList.add('button');
+    innerButtonDiv4.classList.add('button-contents');
+    button4.classList.add('rename');
+    button4.textContent = "Rename";
+    innerButtonDiv4.appendChild(button4);
+    outterButtonDiv4.appendChild(innerButtonDiv4);
+    buttonSection.appendChild(outterButtonDiv4);
 
     // about
     var outterButtonDiv5 = document.createElement('div');
     var innerButtonDiv5 = document.createElement('div');
     var button5  = document.createElement('button');
+    outterButtonDiv5.classList.add('button');
+    innerButtonDiv5.classList.add('button-contents');
+    button5.classList.add('about');
+    button5.textContent = "About";
+    innerButtonDiv5.appendChild(button5);
+    outterButtonDiv5.appendChild(innerButtonDiv5);
+    buttonSection.appendChild(outterButtonDiv5);
 
     // uproot
     var outterButtonDiv6 = document.createElement('div');
     var innerButtonDiv6 = document.createElement('div');
     var button6  = document.createElement('button');
+    outterButtonDiv6.classList.add('button');
+    innerButtonDiv6.classList.add('button-contents');
+    button6.classList.add('uproot');
+    button6.textContent = "Uproot";
+    innerButtonDiv6.appendChild(button6);
+    outterButtonDiv6.appendChild(innerButtonDiv6);
+    buttonSection.appendChild(outterButtonDiv6);
 
-
-
-    currentPlantIndex = 4;
-    document.body.appendChild(plantAndButtonContainer);
-    // allPlantsContainer.appendChild(plantAndButtonContainer);
-    // console.log(allPlantsContainer)
-
-    // console.log(typeof(allPlantsContainer));
+    var plantTitleLink = document.createElement('a')
+    plantTitleLink.textContent = plantName;
+    plantTitleLink.setAttribute('href', "#");
+    plantTitleLink.classList.add('plant-title');
+    // currentPlantIndex = 4;
+    // document.body.appendChild(plantAndButtonContainer);
+    // allPlantsTitle.appendChild(plantTitleLink);
+    // document.body.appendChild(plantTitleLink);
+    allPlantsContainerSection.appendChild(plantAndButtonContainer);
 }
 
 document.getElementById('water').addEventListener('click', waterPlant);
